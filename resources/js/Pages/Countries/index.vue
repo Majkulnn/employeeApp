dac43ab183123ff431901c4ed132b3af31f1ec24
Michał Gałęzyka <majkulnspam@gmail.com>
2023-02-26 00:49:51 +0100

    countries crud

--- resources/js/Pages/Countries/index.vue
+++ resources/js/Pages/Countries/index.vue
@@ -5,16 +5,14 @@ import SearchInput from 'vue-search-input'
 import 'vue-search-input/dist/styles.css'
 import TextInput from "@/Components/TextInput.vue";
 
-defineProps({users : Object,})
-
-const user = usePage().props.user;
+defineProps({countries : Object,})
 
 const form = useForm({
     search: '',
     terms: false,
 });
 const submit = () => {
-    form.get('/users');
+    form.get('/countries');
 };
 </script>
 
@@ -23,40 +21,38 @@ const submit = () => {
 
     <AuthenticatedLayout>
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Users</h2>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Countries</h2>
         </template>
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 lg:w-2/5">
                 <div class="m-auto border-1 flex">
-                    <Link :href="route('users.create')" method="get" as="button" class="bg-emerald-300 font-bold mx-auto overflow-hidden shadow-sm sm:rounded-lg self-center">
-                        &nbsp;Create User&nbsp;
+                    <Link :href="route('countries.create')" method="get" as="button" class="bg-emerald-300 font-bold mx-auto overflow-hidden shadow-sm sm:rounded-lg self-center">
+                        &nbsp;Create country&nbsp;
                     </Link>
                     <form @submit.prevent="submit" class="mx-auto">
-                        <search-input v-model="form.search" placeholder="Search for user..." class="w-1/3 "/>
+                        <search-input v-model="form.search" placeholder="Search for country..." class="w-1/3 "/>
                     </form>
-                    <Link :href="`/users`" method="get" as="button" class="bg-green-200 font-bold mr-auto overflow-hidden sm:rounded-lg self-center">
+                    <Link :href="`/countries`" method="get" as="button" class="bg-green-200 font-bold mr-auto overflow-hidden sm:rounded-lg self-center">
                         Show All
                     </Link>
                 </div>
                 <table class="text-gray-900 mx-auto mt-5 table-fixed pt-0 w-full bg-white rounded-2xls">
                         <thead class="w-full border-b-2  font-bold decoration">
                             <td class="w-1/12">Id</td>
-                            <td>Username</td>
-                            <td>Firstname</td>
-                            <td>Lastname</td>
+                            <td>Code</td>
+                            <td>Name</td>
                             <td>Options</td>
                         </thead>
-                        <tbody v-for="user in users" class="p-6 text-gray-900 border-b-2 truncate">
-                            <td class="truncate">{{user.id}}</td>
-                            <td class="truncate">{{user.username}}</td>
-                            <td class="truncate">{{user.first_name}}</td>
-                            <td class="truncate">{{user.last_name}}</td>
+                        <tbody v-for="country in countries" class="p-6 text-gray-900 border-b-2 truncate">
+                            <td class="truncate">{{country.id}}</td>
+                            <td class="truncate">{{country.country_code}}</td>
+                            <td class="truncate">{{country.name}}</td>
                             <td class="truncate font-bold space-x-2">
-                                <Link :href="`/users/${user.id}/edit`" methods="GET" as="button" class="bg-yellow-200 rounded-md">
+                                <Link :href="`/countries/${country.id}/edit`" methods="GET" as="button" class="bg-yellow-200 rounded-md">
                                     &nbsp;Edit&nbsp;
                                 </Link>
-                                <Link :href="`/users/${user.id}`" method="delete" as="button" class="bg-red-400 rounded-md">
+                                <Link :href="`/countries/${country.id}`" method="delete" as="button" class="bg-red-400 rounded-md">
                                     &nbsp;Delete&nbsp;
                                 </Link>
                             </td>
