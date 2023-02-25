52c03eaabce9026ac3b218fac8525d6d38d84010
Michał Gałęzyka <majkulnspam@gmail.com>
2023-02-25 23:48:33 +0100

    delete password update

--- resources/js/Pages/Users/edit.vue
+++ resources/js/Pages/Users/edit.vue
@@ -18,12 +18,9 @@ const form = useForm('ChangeUser',{
     email: user.email,
     terms: false,
 });
-const formPassword = useForm('ChangePassword',{
-    oldPassword: '',
-    newPassword: '',
-    newPassword_confirmation: '',
-    terms: false,
-});
+const submit = () => {
+    form.put(`/users/${user.id}`);
+};
 </script>
 
 <template>
@@ -34,11 +31,11 @@ const formPassword = useForm('ChangePassword',{
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">Edit User Data</h2>
         </template>
         <div class="py-12">
-            <div class="block max-w-7xl mx-auto sm:px-6 shadow-lg  lg:px-8 w-1/3">
+            <div class="max-w-7xl mx-auto sm:px-6 shadow-lg  lg:px-8 w-1/3">
                 <div>
                     Change Data
                 </div>
-                <form @submit.prevent="form.put(`${user.id}`)">
+                <form @submit.prevent="submit">
                     <div class="card-header">
                         <InputLabel for="username" value="Username" />
 
@@ -98,70 +95,12 @@ const formPassword = useForm('ChangePassword',{
                     </div>
 
                     <div class="flex items-center justify-center space-x-36 mt-4 mx-0">
-                        <Link :href="`/users/${user.id}`" method="delete" as="button" class="bg-red-400 rounded-md">
+                        <Link :href="`/users/${user.id}`" method="delete" as="button" class="rounded-md">
                             <SecondaryButton class="bg-[#dc2626] text-white hover:bg-red-800">
                                 Delete
                             </SecondaryButton>
                         </Link>
-                        <PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                            Update
-                        </PrimaryButton>
-                    </div>
-                </form>
-            </div>
-            <div class="block max-w-7xl mx-auto sm:px-6 shadow-lg  lg:px-8 w-1/3">
-                <div>
-                    Change Password
-                </div>
-                <form @submit.prevent="">
-                    <div>
-                        <InputLabel for="username" value="Username" />
 
-                        <TextInput
-                            id="username"
-                            type="text"
-                            class="mt-1 block w-full"
-                            v-model="formPassword.oldPassword"
-                            required
-                            autocomplete="username"
-                        />
-
-                        <InputError class="mt-2" :message="form.errors.oldPassword" />
-                    </div>
-
-                    <div>
-                        <InputLabel for="firstName" value="Firstname"/>
-
-                        <TextInput
-                            id="firstName" type="text" class="mt-1 block w-full"
-                            v-model="formPassword.newPassword"
-                            required
-                            autocomplite="firsName"
-                        />
-
-                        <InputError class="mt-2" :message="form.errors.newPassword" />
-                    </div>
-                    <div>
-                        <InputLabel for="lastName" value="Lastname" />
-
-                        <TextInput
-                            id="lastName"
-                            type="text"
-                            class="mt-1 block w-full"
-                            v-model="formPassword.newPassword_confirmization"
-                            required
-                            autocomplete="lastName"
-                        />
-
-                        <InputError class="mt-2" :message="form.errors.newPassword_confirmization" />
-                    </div>
-
-                    <div class="flex items-center justify-center space-x-36 mt-4 mx-0">
-                        <Link :href="`/users/${user.id}`" method="delete" as="button" class="bg-red-400 rounded-md">
-                            <SecondaryButton class="bg-[#dc2626] text-white hover:bg-red-800">
-                                Delete
-                            </SecondaryButton>
-                        </Link>
                         <PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
                             Update
                         </PrimaryButton>
